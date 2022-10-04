37b73361ccaa6dc479fbee23a766bd54a057f146
javierperezraffo <javierperezraffo@gmail.com>
2022-10-04 19:34:18 -0300

    BaSP-W07-B22: Conscientiousness corrections

--- Week-07/javascript/login.js
+++ Week-07/javascript/login.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     var inputEmail = document.getElementById('email');
     var inputPassword = document.getElementById('password');
-
     function validateEmail() {
         var email = inputEmail.value.trim();
         var regex = /\S+@\S+\.\S+/;
@@ -15,12 +13,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validatePassword() {
         var password = inputPassword.value.trim();
         var regex = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
         var valid = regex.test(password);
-
         if (valid == true) {
             inputPassword.classList.remove('inputError');
         } else {
@@ -28,28 +24,21 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     inputEmail.addEventListener('blur', function () {
         validateEmail();
     });
-
     inputPassword.addEventListener('blur', function () {
         validatePassword();
     })
-
     inputEmail.addEventListener('focus', function () {
         inputEmail.classList.remove('inputError');
     });
-
     inputPassword.addEventListener('focus', function () {
         inputPassword.classList.remove('inputError');
     });
-
-
     function submitLogin() {
         var email = inputEmail.value;
         var password = inputPassword.value;
-
         fetch('https://basp-m2022-api-rest-server.herokuapp.com/login?email=' + email + '&password=' + password)
             .then((response) => response.json())
             .then((data) => {
@@ -60,29 +49,22 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
     }
-
     document.getElementById('login').addEventListener('click', function (event) {
         event.preventDefault();
-
         var validEmail = validateEmail(inputEmail);
         var validPassword = validatePassword(inputPassword);
-
         if (validEmail == true && validPassword == true) {
             alert('User logged in! Email: ' + inputEmail.value + ' - Password: ' + inputPassword.value);
             submitLogin();
         } else {
             var errors = [];
-
             if (validEmail == false) {
                 errors.push('Email is not valid');
             }
-
             if (validPassword == false) {
                 errors.push('Password is not valid');
             }
-
             alert('Cannot log in: ' + errors.join(', '));
         }
     });
-
 });
--- Week-07/javascript/sign-up.js
+++ Week-07/javascript/sign-up.js
@@ -1,28 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     var storedUser = localStorage.getItem('userInfo');
-
     if (storedUser != undefined) {
         storedUser = JSON.parse(storedUser);
-
         for (var entry of Object.entries(storedUser)) {
-
             var id = entry[0];
             var value = entry[1];
-
             var input = document.getElementById(id);
             if (input != undefined) {
                 input.value = value;
             }
-
             if (id == 'password') {
                 document.getElementById('passwordRepeat').value = value;
             }
-
         }
-
     }
-
     var inputName = document.getElementById('name');
     var inputLastname = document.getElementById('lastName');
     var inputDNI = document.getElementById('dni');
@@ -34,12 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var inputEmail = document.getElementById('email');
     var inputPassword = document.getElementById('password');
     var inputPasswordRepeat = document.getElementById('passwordRepeat');
-
     function validateName() {
         var name = inputName.value.trim();
         var regex = /[a-zA-Z]/g;
         var valid = name.length > 3 && regex.test(name);
-
         if (valid == true) {
             inputName.classList.remove('inputError');
         } else {
@@ -47,12 +36,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validateLastname() {
         var lastname = inputLastname.value.trim();
         var regex = /[a-zA-Z]/g;
         var valid = lastname.length > 3 && regex.test(lastname);
-
         if (valid == true) {
             inputLastname.classList.remove('inputError');
         } else {
@@ -60,11 +47,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validateDNI() {
         var dni = inputDNI.value.trim();
         var valid = dni.length > 7 && !isNaN(dni);
-
         if (valid == true) {
             inputDNI.classList.remove('inputError');
         } else {
@@ -72,12 +57,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validateBirthdate() {
         var birthdate = inputBirthdate.value.trim();
         var regex = /^(0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])[/](19|20)\d\d$/;
         var valid = birthdate.length == 10 && regex.test(birthdate);
-
         if (valid == true) {
             inputBirthdate.classList.remove('inputError');
         } else {
@@ -85,11 +68,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validateTelephone() {
         var telephone = inputTelephone.value.trim();
         var valid = telephone.length == 10 && !isNaN(telephone);
-
         if (valid == true) {
             inputTelephone.classList.remove('inputError');
         } else {
@@ -97,13 +78,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validateAddress() {
         var address = inputAddress.value.trim();
         var regex = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
         var hasSpaceInBetween = address.indexOf(' ') > 0 && address.indexOf(' ') < address.length;
         var valid = address.length > 4 && hasSpaceInBetween && regex.test(address);
-
         if (valid == true) {
             inputAddress.classList.remove('inputError');
         } else {
@@ -111,12 +90,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validateCity() {
         var city = inputCity.value.trim();
         var regex = /[a-zA-Z0-9]/g;
         var valid = city.length > 3 && regex.test(city);
-
         if (valid == true) {
             inputCity.classList.remove('inputError');
         } else {
@@ -124,11 +101,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validatePostalCode() {
         var postalCode = inputPostalCode.value.trim();
         var valid = postalCode.length >= 4 && postalCode.length <= 5 && !isNaN(postalCode);
-
         if (valid == true) {
             inputPostalCode.classList.remove('inputError');
         } else {
@@ -136,12 +111,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validateEmail() {
         var email = inputEmail.value.trim();
         var regex = /\S+@\S+\.\S+/;
         var valid = regex.test(email);
-
         if (valid == true) {
             inputEmail.classList.remove('inputError');
         } else {
@@ -149,12 +122,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validatePassword() {
         var password = inputPassword.value.trim();
         var regex = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
         var valid = password.length >= 8 && regex.test(password);
-
         if (valid == true) {
             inputPassword.classList.remove('inputError');
         } else {
@@ -162,11 +133,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     function validatePasswordRepeat() {
         var passwordRepeat = inputPasswordRepeat.value.trim();
         var valid = validatePassword() && passwordRepeat == inputPassword.value;
-
         if (valid == true) {
             inputPasswordRepeat.classList.remove('inputError');
         } else {
@@ -174,96 +143,72 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return valid;
     }
-
     inputName.addEventListener('blur', function () {
         validateName();
     });
-
     inputLastname.addEventListener('blur', function () {
         validateLastname();
     });
-
     inputDNI.addEventListener('blur', function () {
         validateDNI();
     });
-
     inputBirthdate.addEventListener('blur', function () {
         validateBirthdate();
     });
-
     inputTelephone.addEventListener('blur', function () {
         validateTelephone();
     });
-
     inputAddress.addEventListener('blur', function () {
         validateAddress();
     });
-
     inputCity.addEventListener('blur', function () {
         validateCity();
     });
-
     inputPostalCode.addEventListener('blur', function () {
         validatePostalCode();
     });
-
     inputEmail.addEventListener('blur', function () {
         validateEmail();
     });
-
     inputPassword.addEventListener('blur', function () {
         validatePassword();
     });
-
     inputPasswordRepeat.addEventListener('blur', function () {
         validatePasswordRepeat();
     });
-
-
     inputName.addEventListener('focus', function () {
         inputName.classList.remove('inputError');
     });
-
     inputLastname.addEventListener('focus', function () {
         inputLastname.classList.remove('inputError');
     });
-
     inputDNI.addEventListener('focus', function () {
         inputDNI.classList.remove('inputError');
     });
-
     inputBirthdate.addEventListener('focus', function () {
         inputBirthdate.classList.remove('inputError');
     });
-
     inputTelephone.addEventListener('focus', function () {
         inputTelephone.classList.remove('inputError');
     });
-
     inputAddress.addEventListener('focus', function () {
         inputAddress.classList.remove('inputError');
     });
-
     inputCity.addEventListener('focus', function () {
         inputCity.classList.remove('inputError');
     });
-
     inputPostalCode.addEventListener('focus', function () {
         inputPostalCode.classList.remove('inputError');
     });
-
     inputEmail.addEventListener('focus', function () {
         inputEmail.classList.remove('inputError');
     });
-
     inputPassword.addEventListener('focus', function () {
         inputPassword.classList.remove('inputError');
     });
-
     inputPasswordRepeat.addEventListener('focus', function () {
         inputPasswordRepeat.classList.remove('inputError');
     });
-
     function submitSignUp() {
         var inputs = {
             name: inputName.value,
@@ -277,9 +222,7 @@ document.addEventListener('DOMContentLoaded', function () {
             email: inputEmail.value,
             password: inputPassword.value,
         };
-
         var queryParamsArray = [];
-
         for (var input of Object.entries(inputs)) {
             if (input[0] == 'dob') {
                 var date = new Date(input[1]);
@@ -292,18 +235,13 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             queryParamsArray.push(input[0] + '=' + input[1]);
         }
-
         var queryParams = queryParamsArray.join('&');
-
         fetch('https://basp-m2022-api-rest-server.herokuapp.com/signup?' + queryParams)
             .then((response) => response.json())
             .then((data) => {
                 if (data.success == true) {
-
                     alert('Request was successful!: ' + data.msg);
-
                     localStorage.setItem('userInfo', JSON.stringify(data.data));
-
                 } else {
                     var errorMessage = '';
                     data.errors.forEach((error) => errorMessage += error.msg + '\n');
@@ -311,10 +249,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
     }
-
     document.getElementById('create').addEventListener('click', function (event) {
         event.preventDefault();
-
         submitSignUp();
         var inputs = {
             name: validateName(),
@@ -329,38 +265,29 @@ document.addEventListener('DOMContentLoaded', function () {
             password: validatePassword(),
             passwordRepeat: validatePasswordRepeat(),
         };
-
         var errors = [];
-
         for (var input of Object.entries(inputs)) {
             var inputName = input[0];
             var inputValid = input[1];
-
             if (inputValid == false) {
                 var errorMessage = document.getElementById(inputName).nextElementSibling.innerText;
                 errors.push(errorMessage);
             }
         }
-
         if (errors.length > 0) {
             alert('ERRORS:\n' + errors.join('\n'));
         } else {
-
             var accountInfo = '';
             for (var input of Object.entries(inputs)) {
                 var inputName = input[0];
                 var inputLabel = document.querySelector('label[for="' + inputName + '"]').innerText;
                 var inputValue = document.getElementById(inputName).value;
-
                 if (inputName == 'birthdate') {
                     inputValue = new Date(inputValue).toLocaleDateString('es-AR');
                 }
-
                 accountInfo += inputLabel + ' ' + inputValue + '\n';
             }
-
             alert('User created successfully!\n' + accountInfo);
         }
     });
-
 });
